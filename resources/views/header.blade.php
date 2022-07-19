836aedec79be67b491dc15698ffbf4893e7c1e8a
Muhammad Ahmed <sss.ahm7@gmail.com>
2022-07-19 11:26:43 +0500

    login and logout

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,11 +1,15 @@
 <?php
 use App\Http\Controllers\ProductController;
-$total = ProductController::cartItem();
+
+$total = 0;
+if (Session()->has('user')) {
+    $total = ProductController::cartItem();
+}
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">E-commerce</a>
+        <a class="navbar-brand" href="/">E-commerce</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -13,7 +17,7 @@ $total = ProductController::cartItem();
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Orders</a>
@@ -21,15 +25,39 @@ $total = ProductController::cartItem();
                 <li class="nav-item">
                     <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Shop</a>
                 </li>
+                <li>
+                    <form action="/search" class="d-flex">
+                        <input class="form-control me-2 search-box" name="query" type="search" placeholder="Search"
+                            aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </li>
+                <li>
+                    <button class="btn cart-btn">
+                        <a href="#">cart({{ $total }})</a>
+                    </button>
+                </li>
+                <li>
+                    @if (Session::has('user'))
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ Session::get('user')['name'] }}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="/logout">Logout</a></li>
+                    </ul>
+                </li>
+            @else
+                <li>
+                    <button class="btn">
+                        <a href="/login">Login</a>
+                    </button>
+                </li>
+                @endif
+                </li>
             </ul>
-            <form action="/search" class="d-flex">
-                <input class="form-control me-2 search-box" name="query" type="search" placeholder="Search"
-                    aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-            <button class="btn cart-btn">
-                <a href="#">cart({{ $total }})</a>
-            </button>
+
         </div>
     </div>
 </nav>
